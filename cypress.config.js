33f669defd33b2dcd51215e7c4fd380fc3cd4872
Sreepriya <sreepriya@dhiway.com>
2025-02-27 10:08:26 +0530

    test commit feb27-2

--- cypress.config.js
+++ cypress.config.js
@@ -4,7 +4,7 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
 
-      
+       baseUrl: process.env.CYPRESS_BASE_URL || "http://localhost:5001"
      
       // implement node event listeners here
     },
