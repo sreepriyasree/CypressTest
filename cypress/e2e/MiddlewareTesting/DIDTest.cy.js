37291054cf7603f4cc621e5a02d032a7cc8d41ff
Sreepriya <sreepriya@dhiway.com>
2025-04-07 09:38:10 +0530

    DIDTEST add

---
+++ cypress/e2e/MiddlewareTesting/DIDTest.cy.js
@@ -0,0 +1,25 @@
+describe("GET call using environment variable", () => {
+    it("Fetches data using a valid DID", () => {
+      const did = Cypress.env("did") || "did:web:ashwin.myn.social"; // Default value if env var is not set
+  
+      cy.request({
+        method: "GET",
+        url: `/did/resolve-did-doc?did=${did}`, 
+      }).then((response) => {
+        expect(response.status).to.eq(200);
+        cy.log("Valid DID Response: ", JSON.stringify(response.body));
+      });
+    });
+  
+    it("Fetches data using an invalid DID", () => {
+      cy.request({
+        method: "GET",
+        url: `/did/resolve-did-doc?did=did:web:sree.myn.social`, 
+        failOnStatusCode: false, // Prevents Cypress from failing on 404
+      }).then((response) => {
+        expect(response.status).to.eq(404);
+        cy.log("Invalid DID Response: ", JSON.stringify(response.body));
+      });
+    });
+  });
+  
